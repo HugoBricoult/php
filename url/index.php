9c4c63aae14df692118a16afb21bbdabfc5de908
hugo <hugo.bricoult0521@gmail.com>
2020-02-20 09:17:54 +0100

    ajout challenge url

---
+++ url/index.php
@@ -0,0 +1,17 @@
+<h2>exo 1/2</h2>
+<?php
+if(isset($_GET['nom'])){
+    echo $_GET['nom']."<br>";
+}
+if(isset($_GET['prenom'])){
+    echo $_GET['prenom']."<br>";
+}
+if(isset($_GET['age'])){
+    echo $_GET['age']."<br>";
+}else{
+    echo "age n'as pas été renseigner<br>";
+}
+?>
+<hr>
+<h2>exo 3 /4 /5 /6</h2>
+<p>idem que les autres</p>
